b85c84ab636cbecd6ba7bac7f361f41e7cfcb25f
admin <admin>
2017-01-30 21:06:37 +0000

    Export: exporting channel1

---
+++ Channel1-To-vG2IpYoUsYLjmJ/main.lua
@@ -0,0 +1,6 @@
+-- The main function is the first function called from Iguana.
+-- The Data argument will contain the message to be processed.
+function main(Data)
+   -- this is a simple module
+   -- change 1
+end
